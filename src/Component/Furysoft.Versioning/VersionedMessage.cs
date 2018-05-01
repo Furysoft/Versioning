600c2c6cababc1d867c7aee11ac64e0d75db0f12
Unknown <simongparamore@gmail.com>
2018-05-01 23:34:51 +0100

    Added in Versioned Message

---
+++ src/Component/Furysoft.Versioning/VersionedMessage.cs
@@ -0,0 +1,30 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="VersionedMessage.cs" company="Simon Paramore">
+// © 2017, Simon Paramore
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace Furysoft.Versioning
+{
+    using System.Runtime.Serialization;
+
+    /// <summary>
+    /// The Versioned Message
+    /// </summary>
+    [DataContract]
+    public sealed class VersionedMessage
+    {
+        /// <summary>
+        /// Gets or sets the data.
+        /// </summary>
+        [DataMember(Name = "d", Order = 2)]
+        public string Data { get; set; }
+
+        /// <summary>
+        /// Gets or sets the version.
+        /// </summary>
+        [DataMember(Name = "v", Order = 1)]
+        public DtoVersion Version { get; set; }
+    }
+}
+}
