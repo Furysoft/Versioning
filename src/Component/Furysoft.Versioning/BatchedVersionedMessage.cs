d27558ce1a498dcbc556339cfe85418b2add7ad2
Simon Paramore <simon.paramore@emailhippo.com>
2018-06-25 12:04:24 +0100

    Added in batched versioned messages

---
+++ src/Component/Furysoft.Versioning/BatchedVersionedMessage.cs
@@ -0,0 +1,25 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="BatchedVersionedMessage.cs" company="Simon Paramore">
+// © 2017, Simon Paramore
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace Furysoft.Versioning
+{
+    using System.Collections.Generic;
+    using System.Runtime.Serialization;
+
+    /// <summary>
+    /// The Batched Versioned Message
+    /// </summary>
+    [DataContract]
+    [DtoVersion(typeof(BatchedVersionedMessage), 1, 0, 0)]
+    public sealed class BatchedVersionedMessage
+    {
+        /// <summary>
+        /// Gets or sets the data.
+        /// </summary>
+        [DataMember(Name = "m", Order = 1)]
+        public IEnumerable<VersionedMessage> Messages { get; set; }
+    }
+}
--- src/Component/Furysoft.Versioning/DtoVersion.cs
+++ src/Component/Furysoft.Versioning/DtoVersion.cs
@@ -51,6 +51,23 @@ namespace Furysoft.Versioning
         }
 
         /// <summary>
+        /// Initializes a new instance of the <see cref="DtoVersion"/> class.
+        /// </summary>
+        /// <param name="type">The type.</param>
+        /// <param name="major">The major.</param>
+        /// <param name="minor">The minor.</param>
+        /// <param name="patch">The patch.</param>
+        public DtoVersion([NotNull] string type, int major, int minor, int patch)
+        {
+            this.Type = type;
+            this.Major = major;
+            this.Minor = minor;
+            this.Patch = patch;
+            this.VersionType = VersionType.None;
+            this.VersionTypeVersion = 0;
+        }
+
+        /// <summary>
         /// Initializes a new instance of the <see cref="DtoVersion" /> class.
         /// </summary>
         /// <param name="major">The major.</param>
@@ -79,6 +96,25 @@ namespace Furysoft.Versioning
         public DtoVersion([NotNull] Type type, int major, int minor, int patch, VersionType versionType, int versionTypeVersion)
         {
             this.Type = type.Name;
+            this.Major = major;
+            this.Minor = minor;
+            this.Patch = patch;
+            this.VersionType = versionType;
+            this.VersionTypeVersion = versionTypeVersion;
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="DtoVersion"/> class.
+        /// </summary>
+        /// <param name="type">The type.</param>
+        /// <param name="major">The major.</param>
+        /// <param name="minor">The minor.</param>
+        /// <param name="patch">The patch.</param>
+        /// <param name="versionType">Type of the version.</param>
+        /// <param name="versionTypeVersion">The version type version.</param>
+        public DtoVersion([NotNull] string type, int major, int minor, int patch, VersionType versionType, int versionTypeVersion)
+        {
+            this.Type = type;
             this.Major = major;
             this.Minor = minor;
             this.Patch = patch;
